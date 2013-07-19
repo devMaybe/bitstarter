5c0083169b4c390ab8ea1ef744a71a4e5af5b8d8
Ubuntu <ubuntu@domU-12-31-39-06-69-89.compute-1.internal>
2013-07-19 12:08:26 +0000

    Write SSJS headless grader-2

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util'); 
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://aqueous-oasis-3627.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +64,38 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkUrl = function(url, checksfile) {
+    $ = cheerio.load(url);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <myUrl>','Path to external url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        if (program.url) {
+           rest.get(program.url).on('complete', function(result) {
+              var checkJson = checkUrl(result, program.checks);
+              var outJson = JSON.stringify(checkJson, null, 4);
+              console.log(outJson);
+           });
+        }
+        else if (program.file) {
+           var checkJson = checkHtmlFile(program.file, program.checks);
+           var outJson = JSON.stringify(checkJson, null, 4);
+           console.log(outJson);
+        }
+        else {
+           console.log("Please present a file or an url as parameter.");
+        }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
